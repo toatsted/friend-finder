38f691c6b50b7c1bcc42239fd15272ac6c881fad
Sam <samueljacksonhicks@gmail.com>
2018-04-22 18:38:04 -0500

    home page has jumbotron and button that links to /survey. home.html is no longer at / its at /home. default changed to /home

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,14 @@
 module.exports = function(app, path){
+	app.get("/home", (req, res) => {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+	})
+
 	app.get("/survey", (req, res) => {
 		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	})
 
 	// sets default route as home
 	app.use((req, res) => {
-		res.sendFile(path.join(__dirname, "../public/home.html"));
+		res.redirect("/home");
 	})
 }
