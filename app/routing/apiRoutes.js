bd12b87cacc513ade587d84392839125fd54857a
Sam <samueljacksonhicks@gmail.com>
2018-04-22 21:14:30 -0500

    api get request working with dummy data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,16 @@
-module.exports = function(app){
-	
+module.exports = function(app, friends){
+	app.get("/api/friends", (req, res) => {
+
+		friends = [
+			{
+				name: "idiot1",
+				age: 20
+			},{
+				name: "idiot2",
+				age: 21
+			}
+		]
+
+		res.json(friends);
+	})
 }
